9747d8432106716ea2e4a902b8d702c6c60f2252
Otto <ojornag@gmail.com>
2024-02-24 14:13:13 +0100

    Make eww start automatically on HDMI-A-1

--- .config/hypr/eww_start.sh
+++ .config/hypr/eww_start.sh
@@ -1,2 +1,4 @@
-eww open bar --screen 1
-eww open status_window --screen 1
+monitor_id=$(hyprctl monitors | grep 'HDMI-A-1 (' | sed 's/.*ID \(.\).*/\1/g')
+
+eww open bar --screen $monitor_id
+eww open status_window --screen $monitor_id
