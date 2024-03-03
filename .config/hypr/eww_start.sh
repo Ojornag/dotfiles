dadb72fa259c1493f396b27a136e9a5d9385c37c
Otto <ojornag@gmail.com>
2024-03-03 15:00:35 +0100

    Fixed bug where eww started on the wrong monitor

--- .config/hypr/eww_start.sh
+++ .config/hypr/eww_start.sh
@@ -1,4 +1,6 @@
 monitor_id=$(hyprctl monitors | grep 'HDMI-A-1 (' | sed 's/.*ID \(.\).*/\1/g')
 
+hyprctl dispatch focusmonitor $monitor_id
+
 eww open bar --screen $monitor_id
 eww open status_window --screen $monitor_id
