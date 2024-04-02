a2e3349382795dbc70a5b2a0a998709993ba9799
Otto <ojornag@gmail.com>
2024-04-02 22:11:20 +0200

    Fixed wallpaper randomizer

--- wallpapers/wallpaper.sh
+++ wallpapers/wallpaper.sh
@@ -1,12 +1,16 @@
 categories=("anime" "evangelion" "minimal" "radium" "centered")
 
-category=${categories[$(($RANDOM % ${#categories[@]}))]}
+cat_rand=$RANDOM
+echo "\n\n$cat_rand\n\n"
+category=${categories[$(($cat_rand % ${#categories[@]}))]}
 
-link=$(curl https://api.github.com/repos/dharmx/walls/git/trees/main | jq -r ".tree[] | select(.path==\"$category\")" | jq -r ".url")
+link=$(curl https://api.github.com/repos/dharmx/walls/git/trees/main | jq -r ".tree[] | select(.path==\"$category\") | .url")
 
 dir=$(curl $link)
 
-wallpaper=$(echo $dir | jq -r ".tree[1:][$RANDOM % length].path")
+wall_rand=$RANDOM
+echo "\n\n$wall_rand\n\n"
+wallpaper=$(echo $dir | jq -r ".tree | .[1:][$wall_rand % length].path")
 
 wget "https://github.com/dharmx/walls/raw/main/$category/$wallpaper"
 
