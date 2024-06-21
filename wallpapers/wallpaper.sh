dcca0f206bea6281d6fc6967a37f8bed0590b3ce
Otto <ojornag@gmail.com>
2024-06-21 18:27:49 +0200

    Made so new wallpaper can't be the same as the current one

--- wallpapers/wallpaper.sh
+++ wallpapers/wallpaper.sh
@@ -4,4 +4,11 @@ wallpaper_number=$(($(ls $path -1 | wc -l) - 1))
 
 random_wallpaper=$(($RANDOM % $wallpaper_number))
 
+while [ $random_wallpaper == $1 ]
+do
+	random_wallpaper=$(($RANDOM % $wallpaper_number))
+done
+
+eww update current_wallpaper=$random_wallpaper
+
 swww img --transition-type center $path/$random_wallpaper.*
