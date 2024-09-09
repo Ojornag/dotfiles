2b549684f1f72b2ae9276f600a86006fc35c8ea5
Otto Jorna <ojornag@gmail.com>
2024-09-09 17:50:00 +0200

    Changed scroll and eww initialization for laptop

--- .config/hypr/eww_start.sh
+++ .config/hypr/eww_start.sh
@@ -1,5 +1,9 @@
 monitor_id=$(hyprctl monitors | grep 'HDMI-A-1 (' | sed 's/.*ID \(.\).*/\1/g')
 
+if [ -z "$monitor_id" ]; then
+	monitor_id=0
+fi
+
 hyprctl dispatch focusmonitor $monitor_id
 
 eww open bar --screen $monitor_id
